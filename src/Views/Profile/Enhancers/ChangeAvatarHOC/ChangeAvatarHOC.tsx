7b93ff9fd2353c649388810bc788d7f8c2b3207b
fmakareev <fmakareev@code-artel.com>
2019-07-05 16:09:03 +0300

    Add update user in store. remove console.log

--- src/Views/Profile/Enhancers/ChangeAvatarHOC/ChangeAvatarHOC.tsx
+++ src/Views/Profile/Enhancers/ChangeAvatarHOC/ChangeAvatarHOC.tsx
@@ -1,19 +1,25 @@
 import * as React from 'react';
-import FileUpload from "../FileUpload/FileUpload";
+import FileUpload from "../../../../Enhancers/FileUpload/FileUpload";
 import ChangeAvatarMutation from './ChangeAvatarMutation.graphql';
 import {MutateProps, graphql} from "react-apollo";
-import {IUpdateUserDataVariables, IUpdateUserData} from "../../../../Apollo/schema";
+import {IUpdateUserDataVariables, IUpdateUserData, IUser} from "../../../../Apollo/schema";
 import {IResponseUploadFile} from "../../../Users/Enhancers/CreateReception/CreateReception";
 import {FORM_ERROR} from "final-form";
 import {GetMessageByTranslateKey} from "../../../../Shared/TranslateDict";
 import {connect} from "react-redux";
 import {IStoreState} from "../../../../Store/Store";
 import {IUserState} from "../../../../Store/Reducers/User/reducers";
+import {AddUser} from '../../../../Store/Reducers/User/actionCreators';
+import {Dispatch} from "redux";
+import Logging from "../../../../Helpers/Logging";
+import {ApolloError} from 'apollo-boost';
 
 interface IChangeAvatarHOCProps extends MutateProps {
   uploadFile(file: any): Promise<any>;
 
   user: IUserState;
+
+  updateUser(user: IUser): any;
 
   [prop: string]: any
 }
@@ -22,9 +28,13 @@ const mapStateToProps = (state: IStoreState) => ({
   user: state.user,
 });
 
+const mapDispatchToProps = (dispatch: Dispatch) => ({
+  updateUser: (user: IUser) => dispatch(AddUser(user)),
+});
+
 const ChangeAvatarHOC = (WrapperComponent: React.ElementType) => {
   return graphql<any, IUpdateUserData, IUpdateUserDataVariables>(ChangeAvatarMutation)(
-    connect(mapStateToProps, null)(
+    connect(mapStateToProps, mapDispatchToProps)(
       FileUpload(class extends React.Component<IChangeAvatarHOCProps> {
 
         onSubmit = async (values: any) => {
@@ -32,27 +42,36 @@ const ChangeAvatarHOC = (WrapperComponent: React.ElementType) => {
           const {user, mutate, uploadFile} = this.props;
 
           const fileResult: IResponseUploadFile = await uploadFile(values.avatar && values.avatar.file);
-          console.log(fileResult);
           if (fileResult.message && fileResult.message !== "upload success") {
             return {
               [FORM_ERROR]: GetMessageByTranslateKey(fileResult.message),
             }
           }
 
-          const updateResult = await mutate({
+          const {graphQLErrors, networkErrors, message, data} = await mutate({
             variables: {
               id: user.user && user.user.id,
               avatar: fileResult.file_data.id,
             }
-          });
+          })
+            .catch((error: ApolloError) => {
+              Logging(error.message, 'error');
+              return JSON.parse(JSON.stringify(error));
+            });
 
-          console.log(updateResult);
-
+          if(!data && (graphQLErrors || networkErrors || message)){
+            return {
+              [FORM_ERROR]: GetMessageByTranslateKey(message),
+            }
+          }
+          if (data) {
+            this.props.updateUser(data.updateUser.user);
+          }
         };
 
         render() {
           const {user, result} = this.props;
-          console.log(this.props);
+
           return (<WrapperComponent
             onSubmit={this.onSubmit}
             initialValues={user.user}

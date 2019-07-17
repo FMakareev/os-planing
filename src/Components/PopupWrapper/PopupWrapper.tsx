0813b483a9c2ab6e4f82614b361cd1fe8e6458db
fmakareev <fmakareev@code-artel.com>
2019-07-17 17:57:23 +0300

    Change modal windows to the portal

--- src/Components/PopupWrapper/PopupWrapper.tsx
+++ src/Components/PopupWrapper/PopupWrapper.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import classNames from 'classnames';
 import close from '../../Assets/img/spritesvg/close.svg';
 import {IPopupHoc} from "../../Enhancers/PopupHOC/PopupHOC";
+import ReactDOM from 'react-dom';
 
 interface IPopupWrapperProps extends IPopupHoc {
   className?: string;
@@ -11,33 +12,57 @@ interface IPopupWrapperProps extends IPopupHoc {
 
   [prop: string]: any
 }
+const ModalRoot: any = document.getElementById('modal-root');
 
-const PopupWrapper: React.FC<IPopupWrapperProps> = ({className, children, isOpen, onClose, subtitle, title}) => {
-  return (
-    <div onClick={onClose} className={classNames('popupbg', {
-      'popupbg--open': isOpen
-    })}>
-      <div onClick={(event) => {
-        event.stopPropagation()
-      }} className={classNames("popup", className, {
-        'popup--open': isOpen
+
+
+
+class PopupWrapper extends React.Component<IPopupWrapperProps> {
+  el: any;
+
+  constructor(props: IPopupWrapperProps) {
+    super(props);
+    this.el = document.createElement('div');
+  }
+
+  componentDidMount() {
+    ModalRoot.appendChild(this.el);
+  }
+
+  componentWillUnmount() {
+    ModalRoot.removeChild(this.el);
+  }
+
+  render() {
+    const {className, children, isOpen, onClose, subtitle, title}= this.props;
+
+    return ReactDOM.createPortal(
+      <div onClick={onClose} className={classNames('popupbg', {
+        'popupbg--open': isOpen
       })}>
-        <a onClick={onClose} className="popup__close">
-          <img className={"icon icon-close"} src={close} alt=""/>
-        </a>
-        {
-          title &&
-          <div className="popup__title">{title}</div>
-        }
-        {
-          subtitle &&
-          <div className="popup__date">{subtitle}</div>
-        }
-        {children}
-      </div>
+        <div onClick={(event) => {
+          event.stopPropagation()
+        }} className={classNames("popup", className, {
+          'popup--open': isOpen
+        })}>
+          <a onClick={onClose} className="popup__close">
+            <img className={"icon icon-close"} src={close} alt=""/>
+          </a>
+          {
+            title &&
+            <div className="popup__title">{title}</div>
+          }
+          {
+            subtitle &&
+            <div className="popup__date">{subtitle}</div>
+          }
+          {children}
+        </div>
+      </div>,
+      this.el,
+    );
+  }
+}
 
-    </div>
-  );
-};
 
 export default PopupWrapper;

3e4aa51057ffa74fde08b61bc3b51fd0c6b778bd
fmakareev <fmakareev@code-artel.com>
2019-07-18 18:12:23 +0300

    Change SelectToggleTextField

--- src/Components/Select/SelectToggleTextField.tsx
+++ src/Components/Select/SelectToggleTextField.tsx
@@ -5,6 +5,7 @@ import Close from '../SvgIcons/Close';
 
 interface ISelectToggleProps extends ISelectBaseAPI {
   className?: string;
+
   [prop: string]: any
 }
 
@@ -23,41 +24,52 @@ export const SelectToggleTextField: React.FC<ISelectToggleProps> = ({
                                                                       className,
                                                                       onReset,
                                                                       labelKey,
-                                                                    }) => (
-  <div onClick={onClick} ref={wrapperRef} className={classNames("form__group form__group--select", className)}>
-    <div className="jq-selectbox jqselect form__select">
-      <div className="jq-selectbox__select">
-        <input
-          ref={inputRef}
-          className="jq-selectbox__select-text"
-          type="text"
-          value={meta && meta.focus ? findSubstring : !Array.isArray(value) && value ?value[labelKey]: ''}
-          placeholder={placeholder}
-          onKeyDown={(event) => {
-            onKeyDown && onKeyDown(event.key);
-          }}
-          onChange={(event) => {
-            handleInputChange && handleInputChange(event.target.value);
-          }}
-        />
+                                                                    }) => {
 
-        {
-          value &&
-          <Close onClick={(event: any): void => {
-            event.stopPropagation();
-            onReset && onReset();
-          }}  className={"jq-selectbox__reset"} height={'12px'} width={'12px'}/>
-        }
-        <div className="jq-selectbox__trigger">
-          <div className="jq-selectbox__trigger-arrow"/>
+  return (
+    <div onClick={onClick} ref={wrapperRef} className={classNames("form__group form__group--select", className)}>
+      <div className="jq-selectbox jqselect form__select">
+        <div className="jq-selectbox__select">
+          <div
+            style={{
+              position: 'absolute',
+            }}
+            className="jq-selectbox__select-text"
+          >
+            {meta && meta.focus && findSubstring ? findSubstring : !Array.isArray(value) && value ? value[labelKey] : ''}
+          </div>
+          <input
+            ref={inputRef}
+            className="jq-selectbox__select-text"
+            type="text"
+            value={meta && meta.focus ? findSubstring : ''}
+            placeholder={!Array.isArray(value) && value ? '' : placeholder}
+            onKeyDown={(event) => {
+              onKeyDown && onKeyDown(event.key);
+            }}
+            onChange={(event) => {
+              handleInputChange && handleInputChange(event.target.value);
+            }}
+          />
+
+          {
+            value &&
+            <Close onClick={(event: any): void => {
+              event.stopPropagation();
+              onReset && onReset();
+            }} className={"jq-selectbox__reset"} height={'12px'} width={'12px'}/>
+          }
+          <div className="jq-selectbox__trigger">
+            <div className="jq-selectbox__trigger-arrow"/>
+          </div>
         </div>
+        {
+          children
+        }
       </div>
-      {
-        children
-      }
+      <label className="form__label">{label}</label>
     </div>
-    <label className="form__label">{label}</label>
-  </div>
-);
+  );
+}
 
 export default SelectToggleTextField;

9aa884b1b4222f8818ada18459d859254a287631
fmakareev <fmakareev@code-artel.com>
2019-07-19 13:51:02 +0300

    Change style component Close

--- src/Components/DropFieldFileItem/DropFieldFileItem.tsx
+++ src/Components/DropFieldFileItem/DropFieldFileItem.tsx
@@ -2,30 +2,28 @@ import * as React from 'react';
 import {Close} from '../SvgIcons/Close';
 
 interface IDropFieldFileItemProps {
-	name: string;
-	onClick: any;
+  name: string;
+  onClick: any;
 
-	[prop: string]: any
+  [prop: string]: any
 }
 
 export const DropFieldFileItem: React.FC<IDropFieldFileItemProps> = ({name, onClick}) => (
-	<div className="files-item">
-		<div className="files-item__name">
-			{name}
-		</div>
-		<a
-			onClick={(event) => {
-				event.preventDefault();
-				onClick();
-			}}
-			className="files-item__close"
-			href="#!"
-		>
-			<span className="icon icon-close">
-				<Close height={'8px'} width={'8px'}/>
-			</span>
-		</a>
-	</div>
+  <div className="files-item">
+    <div className="files-item__name">
+      {name}
+    </div>
+    <a
+      onClick={(event) => {
+        event.preventDefault();
+        onClick();
+      }}
+      className="files-item__close"
+      href="#!"
+    >
+      <Close className="icon icon-file-remove" height={'10px'} width={'10px'}/>
+    </a>
+  </div>
 );
 
 export default DropFieldFileItem;

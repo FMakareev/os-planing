57b1ba44f40af33c03ed6ba4e9c82c3c8f678e76
fmakareev <fmakareev@code-artel.com>
2020-01-10 14:19:18 +0300

    add dynamic height

--- src/Views/Calendar/Components/EventList/EventList.tsx
+++ src/Views/Calendar/Components/EventList/EventList.tsx
@@ -4,6 +4,7 @@ import Scrollbars from 'react-custom-scrollbars';
 import EventDetailsCard from '../EventDetailsCard/EventDetailsCard';
 import {IEvent} from "../../../../Apollo/schema";
 import ChangeStatusEvent from '../../Enhancers/ChangeStatusEvent/ChangeStatusEvent';
+import {useEffect, useState} from "react";
 
 interface IEventListProps {
   events: IEvent[];
@@ -14,11 +15,22 @@ interface IEventListProps {
 const EventDetailsCardWithQuery = ChangeStatusEvent(EventDetailsCard);
 
 const EventList: React.FC<IEventListProps> = ({events}) => {
+
+  const [height, setHeight] = useState(window.innerHeight * 0.65);
+
+  useEffect(() => {
+    window.addEventListener('resize', () => {
+      setHeight(window.innerHeight * 0.65)
+    })
+  }, []);
+
   return (
     <div className={'popup__content'}>
-      <Scrollbars style={{
-        height: '630px'
-      }}>
+      <Scrollbars
+        style={{
+          height: height
+        }}
+      >
         {
           events && events.map((event: IEvent, idx: number) => (<EventDetailsCardWithQuery
             key={idx}
